194315b63c44add1bb1d7b7ecd5df3c0dd4b4ec4
minulnimnathbro12345 <125979192+minulnimnathbro12345@users.noreply.github.com>
2025-09-06 17:43:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=dU4QjAiR#Cjc92BzmH9c9cNoBjUGFKgsqAWN_x7gVn616-sc1gvQ' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=NMBjBJBb#eokU5djm08HYlW_NIprGmad05RuIo2sYgsalawvq7nU' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'nadeenmd' : process.env.SESSION_NAME
 };
